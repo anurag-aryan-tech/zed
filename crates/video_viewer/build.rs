967f02c398d594c01168f6d2ca5b35b9ed75ea75
Curious Boy <anurag.aryan.tech@gmail.com>
2026-03-26 10:18:18 +0530

    Completed main file

--- crates/video_viewer/build.rs
+++ crates/video_viewer/build.rs
@@ -1,4 +1,4 @@
-﻿fn main() {
+﻿(fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
         if let Ok(gst_root) = std::env::var("GSTREAMER_1_0_ROOT_MSVC_X86_64") {
             println!("cargo:rustc-link-search=native={}/lib", gst_root);
